27dbb8f0700ac183696356277446239236c17201
supersuryaansh <prod.genx@gmail.com>
2025-02-06 22:24:33 +0530

    standard

--- example.mjs
+++ example.mjs
@@ -1,12 +1,27 @@
 import Autopass from './index.js'
 import Corestore from 'corestore'
-const pass = new Autopass(new Corestore('./pass'))
 
-const inv = await pass.createInvite()
+const store = new Corestore('example/' + process.argv[2])
 
-const pair = Autopass.pair(new Corestore('./another-pass'), inv)
-const anotherPass = await pair.finished()
-await anotherPass.ready()
-await anotherPass.add('a-note', 'hello this is a note')
-console.log(await anotherPass.get('a-note'))
-await anotherPass.remove('a-note')
+let pass = null
+
+if (process.argv[3]) {
+  const pair = Autopass.pair(store, process.argv[3])
+  pass = await pair.finished()
+} else {
+  pass = new Autopass(store)
+  await pass.ready()
+}
+
+if (pass.base.writable) {
+  const inv = await pass.createInvite()
+  console.log('invite', inv)
+}
+
+onupdate()
+pass.on('update', onupdate)
+
+function onupdate () {
+  console.log('db changed, all entries:')
+  pass.list().on('data', console.log)
+}
