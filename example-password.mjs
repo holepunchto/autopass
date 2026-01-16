65cb7a96c7b72f8d82266c348674ffaaf3f68f73
Dominic Cassidy <dominic@cassidy.casa>
2026-01-16 16:08:12 +0200

    support blind-encryption of autobase

---
+++ example-password.mjs
@@ -0,0 +1,37 @@
+import BlindEncryptionSodium from 'blind-encryption-sodium'
+import Autopass from './index.js'
+import Corestore from 'corestore'
+import process from 'process'
+import b4a from 'b4a'
+const store = new Corestore('example/' + process.argv[2])
+
+let pass = null
+
+const encryptionKey = b4a.from('hello world', 'utf-8')
+const password = b4a.fill(32, 'password')
+
+if (process.argv[3]) {
+  const pair = Autopass.pair(store, process.argv[3], {
+    encryptionKey,
+    blindEncryption: new BlindEncryptionSodium(password)
+  })
+  pass = await pair.finished()
+} else {
+  pass = new Autopass(store, {
+    encryptionKey,
+    blindEncryption: new BlindEncryptionSodium(password)
+  })
+  await pass.ready()
+}
+
+if (pass.base.writable) {
+  const inv = await pass.createInvite()
+  console.log('invite', inv)
+}
+onupdate()
+pass.on('update', onupdate)
+
+function onupdate() {
+  console.log('db changed, all entries:')
+  pass.list().on('data', console.log)
+}
